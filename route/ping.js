a91248074b68b793b2f239a82a4f2a3190536bd1
waliu idowu <waliuwaheed2021@gmail.com>
2025-04-28 21:17:06 +0100

    update

--- route/ping.js
+++ route/ping.js
@@ -24,66 +24,66 @@ router.get("/", async (req, res) => {
         });
 
         const transactions = response.data.data;
-        console.log('Total transactions to process', transactions);
+       
       
-        // for (const txn of transactions) {
-        //   const is_Transaction_exist = await FundSchema.findOne({tx_ref: txn.tx_ref});
-        //   if(is_Transaction_exist) {
-        //     console.log(is_Transaction_exist, 'is alredy exist');
-        //     return res.status(200).json({ message: 'Transactions processed successfully' });
+        for (const txn of transactions) {
+          const is_Transaction_exist = await FundSchema.findOne({tx_ref: txn.tx_ref});
+          if(is_Transaction_exist) {
+            console.log(is_Transaction_exist, 'is alredy exist');
+            return res.status(200).json('successfull');
             
-        //   }
-        //   try {
-        //     console.log('Processing transaction:', txn.id);
+          }
+          try {
+            console.log('Processing transaction:', txn.id);
 
-        //     const existing = await Transaction.findOne({ tx_ref: txn.tx_ref });
-        //     if (existing) {
-        //       console.log(`Transaction ${txn.id} already exists, skipping.`);
-        //       return res.status(200).json({ message: 'Transactions processed successfully' });
-        //     }
+            const existing = await Transaction.findOne({ tx_ref: txn.tx_ref });
+            if (existing) {
+              
+              return res.status(200).json('successfull');
+            }
 
-        //     const date = new Date(txn.created_at);
-        //     const formattedDate = `${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')}/${date.getFullYear()} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+            const date = new Date(txn.created_at);
+            const formattedDate = `${String(date.getMonth() + 1).padStart(2, '0')}/${String(date.getDate()).padStart(2, '0')}/${date.getFullYear()} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
 
-        //     const userFound = await Virexscheme.findOne({ email: txn.customer.email }).exec();
-        //     if (!userFound) {
-        //       console.log(`User with email ${txn.customer.email} not found, skipping.`);
-        //       return res.status(200);
-        //     }
+            const userFound = await Virexscheme.findOne({ email: txn.customer.email }).exec();
+            if (!userFound) {
+             
+              return res.status(200).json('successfull');
+            }
 
-        //     // Save new fund
-        //     await savenewFund(
-        //       txn.customer.email,
-        //       txn.tx_ref,
-        //       txn.payment_type,
-        //       'none',
-        //       txn.amount,
-        //       'none',
-        //       txn.status,
-        //       "none",
-        //       formattedDate,
-        //       txn.id,
-        //     );
+            // Save new fund
+            await savenewFund(
+              txn.customer.email,
+              txn.tx_ref,
+              txn.payment_type,
+              'none',
+              txn.amount,
+              'none',
+              txn.status,
+              "none",
+              formattedDate,
+              txn.id,
+            );
 
-        //     if (txn.status === "successful") {
-        //       userFound.walletBalance += txn.amount_settled;
-        //       await userFound.save();
-        //       console.log(`Wallet updated for ${txn.customer.email}`);
-        //     }
+            if (txn.status === "successful") {
+              userFound.walletBalance += txn.amount_settled;
+              await userFound.save();
+             
+            }
 
-        //     console.log(`Transaction ${txn.id} processed successfully.`);
+           
 
-        //   } catch (innerError) {
-        //     console.error(`Error processing transaction ${txn.id}:`, innerError.message);
-        //     // continue to next transaction
-        //   }
-        // }
+          } catch (innerError) {
+            return res.status(200).json('successfull');
+            // continue to next transaction
+          }
+        }
 
-        console.log('All transactions fetched and processed!');
-        return res.status(200).json({ message: transactions });
+       
+        return res.status(200).json('successfull');
 
       } catch (error) {
-        console.error('Error fetching transactions:', error.response ? error.response.data : error.message);
+       
         return res.status(500).json({ error: 'Error fetching transactions' });
       }
 
